39a2693d87cea73b60d81d68ee726ab4df024dbc
pedrohsa <pedroholanda94@gmail.com>
2022-08-25 10:34:41 -0300

    Foi implementado sistema para resgatar as mensagens do servidor e apresentá-las na tela.

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,13 +3,84 @@ function extractData(){
     promise.then(transformData);
 }
 
-function transformData(response){
-    data = response.data;
-    loadData(data);
+function getStatusHTML(messageObject){
+    const statusHTML = `
+    <div class="message-structure color-status">
+        <p>
+            <span class="message-time">(${messageObject.time})</span>
+            <strong class="sender">${messageObject.from}</strong>
+            <span class="action">${''}</span>
+            <strong class="receiver">${''}</strong>
+            <span class="message-content">${messageObject.text}</span>
+        </p>
+    </div>`;
+
+    return statusHTML
 }
 
-function loadData(data){
-    console.log(data);
+function getMessageHTML(messageObject){
+    const messageHTML = `
+    <div class="message-structure color-message">
+        <p>
+            <span class="message-time">(${messageObject.time})</span>
+            <strong class="sender">${messageObject.from}</strong>
+            <span class="action">${'para'}</span>
+            <strong class="receiver">${messageObject.to}:</strong>
+            <span class="message-content">${messageObject.text}</span>
+        </p>
+    </div>`;
+    
+    return messageHTML
+}
+
+function getPrivateHTML(messageObject){
+    const privateHTML = `
+    <div class="message-structure color-private">
+        <p>
+            <span class="message-time">(${messageObject.time})</span>
+            <strong class="sender">${messageObject.from}</strong>
+            <span class="action">${'reservadamente para'}</span>
+            <strong class="receiver">${messageObject.to}:</strong>
+            <span class="message-content">${data[i].text}</span>
+        </p>
+    </div>`;
+    
+    return privateHTML
+}
+
+function transformData(response){
+    console.log(response);
+
+    data = response.data;
+    let content = '';
+
+    for(let i=0; i<data.length;i++){
+
+        switch(data[i].type){
+
+            case 'status':
+                content += getStatusHTML(data[i]);
+                break;
+
+            case 'message':
+                content += getMessageHTML(data[i]);
+                break;
+
+            case 'private':
+                if(data[i].to === username){
+                    content += getPrivateHTML(data[i]);
+                break;
+                }
+
+                break;
+        }
+    }
+
+    loadData(content);
+}
+
+function loadData(content){
+    document.querySelector('.content').innerHTML = content;
 }
 
 extractData();
